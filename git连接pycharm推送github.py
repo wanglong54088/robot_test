482f7fae0f0b55f41678838d041623d06fe57e43
wanglong54088 <1836166651@qq.com>
2020-08-16 23:01:45 +0800

    first commit

--- git连接pycharm推送github.py
+++ git连接pycharm推送github.py
@@ -1,10 +1,8 @@
 # -*- encoding: utf-8 -*-
 """
-@File    : helloword.py
-@Time    : 2020/8/16 22:24
+@File    : git连接pycharm推送github.py
+@Time    : 2020/8/16 22:58
 @Author  : wanglong
 @Email   : 1836166651@qq.com
 @Software: PyCharm
 """
-
-print("helloword")
